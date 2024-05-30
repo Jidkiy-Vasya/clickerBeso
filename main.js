e96a0631047584bcf69f83c0bd11d7c797789c5f
Jidkiy-Vasya <111237673+Jidkiy-Vasya@users.noreply.github.com>
2024-05-31 01:46:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let icon = document.getElementById(`icons`)
 let result = document.getElementById(`res`)
 let on = document.getElementById(`onimg`)
+let vid = document.getElementById(`vid`)
 
 let letter = 0
 
@@ -24,6 +25,35 @@ function plus(){
     timer ++
 }
 
+function checked(){
+    if(letter >= 10 ){
+        icon.src = `img/z.jpg`
+        vid.innerText = "Тип свинки: Z свинка🪖"
+        
+    }
+    if(letter >= 20){
+        icon.src = `img/funny.jpg`
+        vid.innerText = "Тип свинки: Веселая свинка😁"
+    }
+    if(letter >= 30){
+        icon.src = `img/sad.jpg`
+        vid.innerText = "Тип свинки: Грустная свинка😢"
+    }
+    if(letter >= 40){
+        icon.src = `img/V.jpg`
+        vid.innerText = "Тип свинки: V свинка🪖"
+    }
+    if(letter >= 50){
+        icon.src = `img/withmom.jpg`
+        vid.innerText = "Тип свинки: Свинка с мамой🥰"
+    }
+    if(letter >= 69){
+        icon.src = `img/bald.jpg`
+        vid.innerText = "Тип свинки: Лысая свинка🥚👑"
+        document.body.style.background = `burlywood`;
+    }
+
+}
 
 setInterval(plus, 1000)
 
@@ -35,3 +65,5 @@ setInterval(function(){
         smaller()
     }
 }, 1000)
+
+setInterval(checked, 50)
