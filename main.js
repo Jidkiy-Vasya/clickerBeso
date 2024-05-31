8203638ddd538d1966dd5557af05c23188ec2396
Jidkiy-Vasya <111237673+Jidkiy-Vasya@users.noreply.github.com>
2024-05-31 13:10:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ let letter = 0
 let plusss = 1
 let timer = 0
 
+let checker = true
+
 icon.addEventListener("click", function(){
     pocikAudio.play()
     letter += plusss
@@ -18,7 +20,17 @@ icon.addEventListener("click", function(){
     
 })
 
-
+pasx.addEventListener("click", function(){
+    checker = false
+    document.body.style.background = `#b71540`
+    on.innerText = "Вы нашли секретную свинку"
+    vid.innerText = "Тип свинки мертвая свинка🥀💔"
+    icon.src = `img/death.jpg`
+    plusss = 0
+    pasx_svin.play()
+    pasx.style.display = "none";
+    
+})
 
 function bigger (){
     on.style.fontSize = `20px`
@@ -33,67 +45,67 @@ function plus(){
 }
 
 function checked(){
-    if(letter >= 10 ){
+    if(letter >= 10 && checker){
         icon.src = `img/z.jpg`
         vid.innerText = "Тип свинки: Z свинка🪖"
         
     }
-    if(letter >= 20){
+    if(letter >= 20 && checker){
         icon.src = `img/funny.jpg`
         vid.innerText = "Тип свинки: Веселая свинка😁"
         
     }
-    if(letter >= 30){
+    if(letter >= 30 && checker){
         icon.src = `img/sad.jpg`
         vid.innerText = "Тип свинки: Грустная свинка😢"
     }
-    if(letter >= 40){
+    if(letter >= 40 && checker){
         icon.src = `img/V.jpg`
         vid.innerText = "Тип свинки: V свинка🪖"
     }
-    if(letter >= 50){
+    if(letter >= 50 && checker){
         icon.src = `img/withmom.jpg`
         vid.innerText = "Тип свинки: Свинка с мамой🥰"
     }
-    if(letter >= 60){
+    if(letter >= 60 && checker){
         icon.src = `img/blat.jpg`
         vid.innerText = "Тип свинки: Блатная свинка😎"
         
     }
-    if(letter >= 75){
+    if(letter >= 75 && checker){
         icon.src = `img/famous.jpg`
         vid.innerText = "Тип свинки: Важная свинка🐷😎"
 
         
         plusss = 2
     }
-    if(letter >= 100){
+    if(letter >= 100 && checker){
         icon.src = `img/fat.jpg`
         vid.innerText = "Тип свинки: Толстая свинка🍔"
         
     }
-    if(letter >= 130){
+    if(letter >= 130 && checker){
         icon.src = `img/lit.jpg`
         vid.innerText = "Тип свинки:Cвинка c Lit Energy💦"
         
     }
-    if(letter >= 170){
+    if(letter >= 170 && checker){
         icon.src = `img/love.jpg`
         vid.innerText = "Тип свинки: Влюбленная свинка😍"
         
     }
-    if(letter >= 200){
+    if(letter >= 200 && checker){
         icon.src = `img/sleep.jpg`
         vid.innerText = "Тип свинки: Спящая свинка😴"
         
         plusss = 3
     }
-    if(letter >= 250){
+    if(letter >= 250 && checker){
         icon.src = `img/withZ.jpg`
         vid.innerText = "Тип свинки: Свинка с Андреем Z🪖"
         
     }
-    if(letter >= 310){
+    if(letter >= 310 && checker){
         icon.src = `img/yacht.jpg`
         vid.innerText = "Тип свинки: Нахуй с яхты свинья🤣"
         
@@ -129,14 +141,3 @@ setInterval(function(){
 }, 1000)
 
 setInterval(checked, 50)
-
-
-pasx.addEventListener("click", function(){
-    document.body.style.background = `#b71540`
-    on.innerText = "Вы нашли секретную свинку"
-    vid.innerText = "Тип свинки мертвая свинка🥀💔"
-    icon.src = `img/death.jpg`
-    plusss = 0
-    pasx_svin.play()
-    pasx.style.display = "none";
-})
